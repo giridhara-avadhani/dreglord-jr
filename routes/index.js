7343c98601673eaec334ed2b0dbdc025375738d0
Giridhara Avadhany <giridhara.avadhany@amadeus.com>
2021-07-03 21:54:52 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,16 @@ router.get('/auth/bungie', passport.authenticate('bungie-auth'));
 router.get('/auth/bungie/callback', passport.authenticate('bungie-auth', {
   failureRedirect: '/auth/provide' }), function(req, res) {
     // Successful authentication, redirect home.
-    https.get({
-      hostname: 'https://www.bungie.net/Platform/User/GetMembershipsById/' + req.session.passport.user.membershipId + '/1/',
+    const options = {
+      hostname: 'www.bungie.net',
+      port: 443,
+      path: '/Platform/User/GetMembershipsById/' + req.session.passport.user.membershipId + '/1/',
+      method: 'GET',
       headers: {
         "X-API-Key": '2f2c53a675ac4c56a55f893a026ee040'
       }
-    }, function (error, response, body) {
+    }
+    https.request(options, function (error, response, body) {
         res.render('index', { title: 'Express',  user: JSON.stringify(body)});
         if (!error && response.statusCode === 200) {
             console.log(body) // Print the google web page.
