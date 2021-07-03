ddbb9010181e8e3ebd5e9d978ef9d93f5ead1142
Giridhara Avadhany <giridhara.avadhany@amadeus.com>
2021-07-03 20:07:40 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/auth/bungie', passport.authenticate('bungie-auth'));
 router.get('/auth/bungie/callback', passport.authenticate('bungie-auth', {
   failureRedirect: '/auth/provide' }), function(req, res) {
     // Successful authentication, redirect home.
-    console.log(res.user);
+    console.log(JSON.stringify(res));
     res.redirect('/');
   });
 
