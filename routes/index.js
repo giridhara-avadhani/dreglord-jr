5e44f2d0e78a9d815c04c123416014fd6201793f
Giridhara Avadhany <giridhara.avadhany@amadeus.com>
2021-07-03 20:50:59 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,16 @@ var passport = require('passport');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var user = "";
+  console.log(req.session);
   if (!req.session || !req.session.passport || !req.session.passport.user  || !req.session.passport.user.membershipId) {
     res.render('index', { title: 'Express',  user: user });
   } else {
-    console.log(req.session);
     https.get({
       hostname: 'https://www.bungie.net/Platform/Destiny2/User/GetBungieNetUserById/' + req.session.passport.user.membershipId,
       headers: {
         "X-API-Key": '2f2c53a675ac4c56a55f893a026ee040'
       }
-    },function (error, response, body) {
+    }, function (error, response, body) {
         res.render('index', { title: 'Express',  user: JSON.stringify(body)});
         if (!error && response.statusCode === 200) {
             console.log(body) // Print the google web page.
@@ -28,7 +28,6 @@ router.get('/auth/bungie', passport.authenticate('bungie-auth'));
 router.get('/auth/bungie/callback', passport.authenticate('bungie-auth', {
   failureRedirect: '/auth/provide' }), function(req, res) {
     // Successful authentication, redirect home.
-    console.log(req.session);
     res.redirect('/');
   });
 
